2e6717aa9900a8d7516292ac468ff7544aa74328
Lin Weihong <664121063@qq.com>
2022-04-20 13:34:22 +0800

    First commit

---
+++ src/main/java/com/lin/GiteTest.java
@@ -0,0 +1,11 @@
+package com.lin;
+
+/**
+ * @author Created by Lin Weihong
+ * @date on 2022/4/20 13:28
+ */
+public class GiteTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
